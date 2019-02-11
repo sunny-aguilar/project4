5f4d7ded49a55051c95c4293f983730c85c1a8f2
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-11 13:16:49 -0800

    project4

---
+++ game.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Sandro on 2/11/2019.
+//
+
+#ifndef PROJECT4_GAME_HPP
+#define PROJECT4_GAME_HPP
+
+#endif //PROJECT4_GAME_HPP
---
+++ menu.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Sandro on 2/11/2019.
+//
+
+#ifndef PROJECT4_MENU_HPP
+#define PROJECT4_MENU_HPP
+
+#endif //PROJECT4_MENU_HPP
