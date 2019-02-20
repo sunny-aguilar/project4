edfd7c22482e2edef1197afe0377548cb8ba3d6d
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-19 23:47:05 -0800

    project4

--- heroList.cpp
+++ heroList.cpp
@@ -236,7 +236,7 @@ void HeroList::addLoser(HeroNode *&loserTeam) {
 
 /*********************************************************************
 ** Description:     calculates health to restore. Takes in a constant
- *                  player hero and calculates the max health a
+**                  player hero and calculates the max health a
  *                  player can have. Then health to restore is
  *                  randomly selected and calculated. Functions
  *                  returns the total player health to restore.
