6ebee4b70398b7199015e2c698bed356d76dce0b
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-19 23:57:24 -0800

    project4

--- heroList.cpp
+++ heroList.cpp
@@ -59,12 +59,10 @@ void HeroList::addHeroes(Player player, int heroType, string heroName) {
         case FIRSTPLAYER:
             // assign pickHero to the right team list
             addHeroNode(heroType, heroName, headTeamOne);
-            
-            traverseForward(headTeamOne);
+            traverseForward(headTeamOne);   // delete this function
             break;
         case SECONDPLAYER:
             addHeroNode(heroType, heroName, headTeamTwo);
-
             traverseForward(headTeamTwo);
             break;
         default:
