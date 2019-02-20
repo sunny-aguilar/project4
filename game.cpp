8ca8f17f5254c3114f7333e403eb162e361cc0bc
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-19 23:54:20 -0800

    project4

--- game.cpp
+++ game.cpp
@@ -77,6 +77,7 @@ void Game::gameFlow() {
 
         // create combatant dice
         // auto created inside hero classes in constructor
+        // memory leak??????
 
         // start combat
         //heroes.startTournament();
