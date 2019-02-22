e70250863a9265e7a8fd236945d2127ff4087af9
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-22 12:38:51 -0800

    project4

--- heroList.cpp
+++ heroList.cpp
@@ -232,16 +232,11 @@ void HeroList::addLoser(HeroNode *&loserTeam) {
 
     if (losersList == nullptr) {
         losersList = head;
-        cout << "First loser added to loser list\n";
     }
     else {
         HeroNode *loserHead = losersList;
-        cout << "Old loser head is " << losersList->hero->getName() << endl;
-        cout << "New loser to add on stack " << head->hero->getName() << endl;
         head->next = loserHead;
         losersList = head;
-
-        cout << "Traverse loser list\n";
         traverseForward(losersList);
 
     }
